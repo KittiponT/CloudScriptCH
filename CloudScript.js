4da7fd04a11a9584433f939c07c3c29e0f509e40
DESKTOP-HUCDK5Q\Book <kittipont@zerobit.co.th>
2018-07-18 14:59:11 +0700

    use parseInt

--- CloudScript.js
+++ CloudScript.js
@@ -464,8 +464,8 @@ handlers.checkSaleReporter = function(args,context)
 	  for(var i = 0; i < arrKeyLevel.length;i++)
 	  {
 		 
-		  var currentInt = Number(currentExpLevel);
-		  var loopNumber = Number(dictLevel[arrKeyLevel[i]].Value);
+		  var currentInt = parseInt(currentExpLevel);
+		  var loopNumber = parseInt(dictLevel[arrKeyLevel[i]].Value);
 		  log.debug("currentInt:" +currentInt);
 		  log.debug("loopNumber:" +loopNumber);
 		currentExpLevel = currentInt + loopNumber ;
